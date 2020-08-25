b13edbfbc83a6c390d7478c6d2686a5a20ccb9ed
Ess91 <eagyare91@gmail.com>
2020-08-25 12:15:49 +0100

    api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1> Weather React App</h1>
       <Weather />
       <footer>
         This project was coded by {" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState} from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
+    const [ready, setReady] = useState(false);
+    const [temperature, setTemperature] = useState(null);
+    function handleResponse(response) {
+        console.log(response.data);
+        setTemperature(response.data.main.temp)
+    }
+
+
+
+
+
+
+
+  const apiKey = "c788fbd12920cbf73a67468fe8b0facb";
+  let city = "London"
+  let apiUrl =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
   return (
     <div className="Weather">
       <form>
-          <div className="row">
-              <div className="col-9">
-        <input
-          type="search"
-          placeholder="Enter City.."
-          className="form-control"
-          autoFocus="on"
-          autoComplete="off"
-        />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter City.."
+              className="form-control"
+              autoFocus="on"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
-        <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary w-100"/>
-     </div>
-      </div>
-      
       </form>
       <h1>London</h1>
       <ul>
@@ -29,16 +51,16 @@ export default function Weather() {
 
       <div className="row mt-3">
         <div className="col-6">
-            <div className="clearfix">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-            className="float-left"
-          />
-          <div className="float-left">
-          <span className="temperature">17</span>
-          <span className="units">°C</span>
-          </div>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">17</span>
+              <span className="units">°C</span>
+            </div>
           </div>
         </div>
         <div className="col-6">
