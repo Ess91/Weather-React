8ad7de722e88b64c8a89f6a2690c3976e11a1617
Ess91 <eagyare91@gmail.com>
2020-08-28 00:21:00 +0100

    format date

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -15,12 +15,12 @@ export default function FormatDate(props) {
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
     if (hours < 10) {
-        hours = `0{hours}`;
+        hours = `0${hours}`;
     }
 
   let minutes = props.date.getMinutes();
     if (minutes < 10) {
-    minutes = `0{minutes}`;
+    minutes = `0${minutes}`;
 }
 
 
