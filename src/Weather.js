72c1117f8e37a8eb5a1b66609777912229f23a32
Ess91 <eagyare91@gmail.com>
2020-08-29 00:01:25 +0100

    final changes to project

--- src/Weather.js
+++ src/Weather.js
@@ -73,7 +73,7 @@ export default function Weather(props) {
          color="#000000"
          height={100}
          width={100}
-         timeout={3000}
+         timeout={5000}
  
       />
      );
